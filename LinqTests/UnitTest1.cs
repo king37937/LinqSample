704a671ad289c2b97ccf224af3f167fb19606c6f
king.ho99 <king.ho@titansoft.com.sg>
2018-04-28 10:10:22 +0800

    add MyOwnWhere

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
@@ -12,11 +13,28 @@ namespace LinqTests
         public void find_products_that_price_between_200_and_500()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = WithoutLinq.FindProductByPrice(products, 200, 500);
+            var actual = WithoutLinq.FindProductByPrice(products, 200, 500, "Odd-e");
 
             var expected = new List<Product>()
             {
-                //todo
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void MyOwnLINQ_Where()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = YourOwnLinq.MyOwnWhere(products, 
+                p => p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e");
+
+            var expected = new List<Product>()
+            {
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -26,12 +44,35 @@ namespace LinqTests
 
 internal class WithoutLinq
 {
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
+    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary,
+        string supplier)
     {
-        throw new System.NotImplementedException();
+        var matchedProducts = new List<Product>();
+        foreach (var product in products)
+        {
+            if (product.Price >= lowBoundary && product.Price <= highBoundary && product.Supplier.Equals(supplier))
+            {
+                matchedProducts.Add(product);
+            }
+        }
+
+        return matchedProducts;
     }
 }
 
 internal class YourOwnLinq
 {
+    public static List<Product> MyOwnWhere(IEnumerable<Product> products, Func<Product, bool> func)
+    {
+        var matchedProducts = new List<Product>();
+        foreach (var product in products)
+        {
+            if (func(product))
+            {
+                matchedProducts.Add(product);
+            }
+        }
+
+        return matchedProducts;
+    }
 }
