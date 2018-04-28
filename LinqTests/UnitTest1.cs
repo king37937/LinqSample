ecece98d5ae2cb5293f04d82a0c251a7f57d2cbc
king.ho99 <king.ho@titansoft.com.sg>
2018-04-28 14:52:23 +0800

    add MyTake

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -4,6 +4,7 @@ using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
 using TiIEnumerableExtension;
 
 namespace LinqTests
@@ -32,25 +33,20 @@ namespace LinqTests
             var products = RepositoryFactory.GetProducts();
             var actual = products.MyOwnWhere(p => p.Price >= 200 && p.Price <= 500 && p.Supplier == "Odd-e");
 
-            foreach (var item in actual)
-            {
-                Console.WriteLine(item.Price);
-            }
+           
             var expected = new List<T>()
             {
                 new T{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
                 new T{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
         [TestMethod]
         public void MyOwnLINQ_Where_GetEmployee()
         {
             var employees = RepositoryFactory.GetEmployees();
-            //var actual = YourOwnLinq.MyOwnWhere(
-            //    employees, e => e.Age >= 25 && e.Age <= 40).ToList();
             var actual = employees.MyOwnWhere(e => e.Age >= 25 && e.Age <= 40).ToList();
 
             var expected = new List<Employee>()
@@ -62,6 +58,86 @@ namespace LinqTests
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void ConvertToHttpsTest()
+        {
+            var urls = RepositoryFactory.GetUrls();
+            var actual = urls.MySelect(url => url.Replace("http:", "https:") + ":80");
+
+            var expected = new List<string>()
+            {
+                "https://tw.yahoo.com:80",
+                "https://facebook.com:80",
+                "https://twitter.com:80",
+                "https://github.com:80",
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void UrlLengthTest()
+        {
+            var urls = RepositoryFactory.GetUrls();
+            var actual = urls.MySelect(url=>url.Length);
+
+            var expected = new List<int>()
+            {
+                19, 20, 19, 17
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void Url_tw_append_91_Test()
+        {
+            var urls = RepositoryFactory.GetUrls();
+            var actual = YourOwnLinq.appendPort(urls, url => url.Contains("tw") ? url + ":91" : url);
+
+            var expected = new List<string>()
+            {
+                "http://tw.yahoo.com:91",
+                "https://facebook.com",
+                "https://twitter.com:91",
+                "http://github.com"
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void FindEmployeeTest()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.MyOwnWhere(e => e.Role == RoleType.Engineer).
+                MySelect(e => e.MonthSalary);
+
+            foreach (var item in actual)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            var expected = new List<int>()
+            {
+                100, 140, 280, 120, 250,
+            };
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void MyTakeTest()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.MyTake(3);
+
+            var expected = new List<T>()
+            {
+                new T{Id=1, Cost=11, Price=110, Supplier="Odd-e" },
+                new T{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
+                new T{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
     }
 }
@@ -120,6 +196,33 @@ namespace TiIEnumerableExtension
                 {
                     yield return item;
                 }
+            }
+        }
+
+        public static IEnumerable<TResult> MySelect<TSouce, TResult>(this IEnumerable<TSouce> urls, Func<TSouce, TResult> selector)
+        {
+            foreach (var url in urls)
+            {
+                yield return selector(url);
+            }
+        }
+
+        public static IEnumerable<string> appendPort(IEnumerable<string> urls, Func<string, string> selector)
+        {
+            foreach (var url in urls)
+            {
+                yield return selector(url);
+            }
+        }
+
+        public static IEnumerable<TSource> MyTake<TSource>(this IEnumerable<TSource> source, int top)
+        {
+            var enumerator = source.GetEnumerator();
+
+            while (top > 0 && enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+                top--;
             }
         }
     }
